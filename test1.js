d57845ed7828e9656f5cd8be6fee4e239a7524a4
kishorevendi <vvkb6869@gmail.com>
2021-06-12 12:17:14 +0800

    init

---
+++ test1.js
@@ -0,0 +1,14 @@
+var x=25;
+function my(a,b)
+{
+	if(a>=b)
+	{
+		document.getELementById("demo").innerHTML="IT IS A GREATEST NUMBER" + a;
+	}
+	else
+	{
+		documnet.getElementById("demo").innerHTML="IT'S NOT GREATER VALUE";
+	}
+	document.write(x);
+}
+
